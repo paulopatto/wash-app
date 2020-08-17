6b1e8dfad6b3acf522cb4e6c14b6864fc07c43ac
Paulo Patto <paulopatto@paulopatto.com>
2020-08-17 18:20:21 -0300

    remove dummy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'dummy#home'
+  root to: 'pages#home'
 
-  get 'dummy/home'
-  get 'dummy/blog'
-  get 'dummy/contact'
-  get 'dummy/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
